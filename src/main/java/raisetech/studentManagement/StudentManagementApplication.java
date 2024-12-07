6af45020358ec416790ee5c1a1f84731e4c4f233
Ayano Nakagawa <l2408851201as@gmail.com>
2024-12-07 16:43:29 +0900

    Apache Commons Lang 追加

--- src/main/java/raisetech/studentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/studentManagement/StudentManagementApplication.java
@@ -1,24 +1,49 @@
 package raisetech.studentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.catalina.util.StringUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+  private Map<String, String> students = new HashMap<>();
+
+  {
+    students.put("Dale Cooper", "33");
+
+  }
+
   public static void main(String[] args) {
     SpringApplication.run(StudentManagementApplication.class, args);
 
+
   }
 
-  @GetMapping("/lovely")
-  public String hello() {
-    StringUtils.isEmpty();
-    return "Take it easy, Ayano!";
+  @GetMapping("/studentInfo")
+  public String getStudentInfo(@RequestParam String name) {
+    String age = students.get(name);
+    return age != null ? name + " is " + age + " years old." : "Student not found.";
+  }
+
+  @PostMapping("/studentInfo")
+  public String setStudentInfo(@RequestParam String name, @RequestParam String age) {
+    students.put(name, age);
+    return "Added:" + name + age + " years old.";
+
   }
 }
+
+
+
+
+
+
