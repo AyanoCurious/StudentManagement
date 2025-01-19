0819ae13df698a46add4530ffc9e7fdb889398e7
Ayano Nakagawa <l2408851201as@gmail.com>
2025-01-19 17:03:35 +0900

    students_coursesのオブジェクト作成と全件検索の実装

--- src/main/java/raisetech/studentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/studentManagement/StudentManagementApplication.java
@@ -1,16 +1,10 @@
 package raisetech.studentManagement;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -24,51 +18,25 @@ public class StudentManagementApplication {
     SpringApplication.run(StudentManagementApplication.class, args);
   }
 
-  @GetMapping("/studentInfo")
-  public String getStudentInfo(@RequestParam String name) {
-    Student student = repository.searchByName(name);
-    if (student != null) {
-      return student.getName() + " " + student.getAge() + "歳";
-    } else {
-      return "Student not found.";
-    }
+
+  @GetMapping("/students")
+  public List<Student> getStudentInfoList() {
+    return repository.searchStudent();
+
   }
 
-  @PostMapping("/students")
-  public String createStudentInfo(@RequestParam String name, @RequestParam int age) {
-    Student student = new Student(name, age);
-    repository.registerStudent(student);
-    return "Added: " + name + " " + age + " years old.";
-  }
-
-  @GetMapping("/allStudents")
-  public List<Student> getAllStudents() {
-    return repository.findAll();
-  }
-
-  @PutMapping("/studentInfo")
-  public String updateStudentInfo(@RequestParam String name, @RequestParam int age) {
-    Student student = repository.searchByName(name);
-    if (student != null) {
-      student.setAge(age);
-      repository.update(student);
-      return "Updated: " + name + " " + age + " years old.";
-    } else {
-      return "Student not found.";
-    }
-  }
-
-  @DeleteMapping("/studentInfo")
-  public String deleteStudentInfo(@RequestParam String name) {
-    Student student = repository.searchByName(name);
-    if (student != null) {
-      repository.delete(student); // deleteメソッドをStudentRepositoryに追加
-      return "Deleted: " + name;
-    } else {
-      return "Student not found.";
-    }
+  @GetMapping("/studentsCourses")
+  public List<StudentsCourses> getStudentCourseInfoList() {
+    return repository.searchStudentCourse();
   }
 }
+
+
+
+
+
+
+
 
 
 
